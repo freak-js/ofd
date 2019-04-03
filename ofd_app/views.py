ffcd10b84a148088f93e54cec26c69ffb9b9abf3
manujli <KBerlin@yandex.ru>
2019-04-03 23:31:22 +0400

    provide user info to order template

--- ofd_app/views.py
+++ ofd_app/views.py
@@ -191,7 +191,7 @@ def orders(request):
         products = []
         for rel in rels:
             products.append({'product_name': rel.product.product_name, 'amount': rel.amount, 'cost': rel.cost, 'full_cost': rel.amount * rel.cost})
-        order_data.append({'id': order.id, 'order_num': cnt, 'adddate': order.adddate, 'cnt_products': len(rels), 'total': total, 'comment': order.comment, 'products': products, 'status': order.status.code})
+        order_data.append({'id': order.id, 'order_num': cnt, 'adddate': order.adddate, 'cnt_products': len(rels), 'total': total, 'comment': order.comment, 'products': products, 'status': order.status.code, 'user': order.user})
     filters = {}
     if request.user.is_superuser or request.user.is_admin():
         filters['org'] = User.get_organizations()
