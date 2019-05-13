fd639941cea2c1d9b9a06988fc2a0c43a4797934
freak-js <mahrov.web@gmail.com>
2019-05-13 21:28:01 +0300

    поправил константы

--- ofd_app/constants.py
+++ ofd_app/constants.py
@@ -4,9 +4,9 @@ ORDERS = 'orders'
 MY_CARD = 'my_card'
 STAT = 'stat'
 
-NEW_LOGIN_USER_SUBJECT = 'Добро пожаловать *username* !'
-NEW_LOGIN_USER_BODY = 'Вы успешно зарегестрировались на платформе mir-ofd.ru - как *login*. Наш менеджер свяжится с вами в ближайшее время.'
+TEMPLATE_EMAIL_NEW_LOGIN_USER_SUBJECT = 'Добро пожаловать *username* !'
+TEMPLATE_EMAIL_NEW_LOGIN_USER_BODY = 'Вы успешно зарегестрировались на платформе mir-ofd.ru - как *login*. Наш менеджер свяжится с вами в ближайшее время.'
 
-NEW_LOGIN_ADMIN_SUBJECT = 'Новый пользователь!'
-NEW_LOGIN_ADMIN_BODY = 'Пользователь *имя* *фамилия* почта:*почта* телефон:*телефон* организация:** город:**'
+TEMPLATE_EMAIL_NEW_LOGIN_ADMIN_SUBJECT = 'Новый пользователь!'
+TEMPLATE_EMAIL_NEW_LOGIN_ADMIN_BODY = 'Пользователь *имя* *фамилия* почта:*почта* телефон:*телефон* организация:** город:**'
 
