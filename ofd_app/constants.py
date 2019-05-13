5a21e3c855892157ba7af4f1b9ea4536b3fe7c51
freak-js <mahrov.web@gmail.com>
2019-05-13 21:16:15 +0300

    добавлены константы для отправки писем

--- ofd_app/constants.py
+++ ofd_app/constants.py
@@ -3,3 +3,10 @@ USERS = 'users'
 ORDERS = 'orders'
 MY_CARD = 'my_card'
 STAT = 'stat'
+
+NEW_LOGIN_USER_SUBJECT = 'Добро пожаловать *username* !'
+NEW_LOGIN_USER_BODY = 'Вы успешно зарегестрировались на платформе mir-ofd.ru - как *login*. Наш менеджер свяжится с вами в ближайшее время.'
+
+NEW_LOGIN_ADMIN_SUBJECT = 'Новый пользователь!'
+NEW_LOGIN_ADMIN_BODY = 'Пользователь *имя* *фамилия* почта:*почта* телефон:*телефон* организация:** город:**'
+
