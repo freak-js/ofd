823fd9e8ba8676e52e72d6d00736636b6038ae12
manujli <KBerlin@yandex.ru>
2019-05-26 23:54:25 +0300

    security improve.

--- ofd/settings.py
+++ ofd/settings.py
@@ -24,6 +24,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE¶ = True
 
 ALLOWED_HOSTS = ['.mir-ofd.ru', '5.253.61.163', 'localhost']
 
