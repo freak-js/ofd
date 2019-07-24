d617f5146d05e5317eadc032d436bfa925a64c94
freak-js <mahrov.web@gmail.com>
2019-07-25 02:27:39 +0300

    Небольшая доработка алгоритма пагинации

--- ofd_app/utils.py
+++ ofd_app/utils.py
@@ -15,14 +15,14 @@ def get_pages_list(number_of_pages, current_page):    #Функция отдаю
 
         return range(1, number_of_pages + 1)
 
-    if number_of_pages > 11 and current_page < 6:
+    if number_of_pages > 11 and current_page < 7:
         result.extend(lst[:9])
         result.append('...')
         result.append(last_page)
         
         return result
     
-    if number_of_pages > 11 and current_page >= 6 and current_page not in lst[last_page - 4:last_page + 1]:
+    if number_of_pages > 11 and current_page >= 7 and current_page not in lst[last_page - 6:last_page + 1]:
         result.append(first_page)
         result.append('...')
         result.extend(lst[current_page - 4:current_page + 3])
@@ -31,7 +31,7 @@ def get_pages_list(number_of_pages, current_page):    #Функция отдаю
         
         return result
     
-    if number_of_pages > 11 and current_page >= 6 and current_page in lst[last_page - 4:last_page + 1]:
+    if number_of_pages > 11 and current_page >= 7 and current_page in lst[last_page - 6:last_page + 1]:
         result.append(first_page)
         result.append('...')
         result.extend(lst[last_page - 9:last_page + 1])
